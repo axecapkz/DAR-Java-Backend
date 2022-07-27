9905c5d88d8f4f2e8c9524f750abcc81464ac8b3
Azamat <azamatk@me.com>
2022-07-27 10:59:44 +0600

    update

--- Homework - 1/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-
-        public static void main(String[] args) throws Exception {
-            char[] word = {'w','o','r','d'};
-            System.out.println(word.length);
-        }
-    }
